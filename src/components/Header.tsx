e6729069545983e6faf49d81beda73e733d436fe
jamiebenstead <jamie.benstead@gmail.com>
2024-10-28 12:29:00 +0000

    Add content and styling to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,25 +5,37 @@ import {
   Text,
   VStack,
   useColorMode,
-  Button,
+  Box,
+  IconButton,
+  HStack,
+  Icon,
+  useColorModeValue,
 } from "@chakra-ui/react";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+import { BsLinkedin } from "react-icons/bs";
 
 const Header = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const hoverColor = useColorModeValue("gray.700", "gray.300");
 
   return (
     <>
-      <Button onClick={toggleColorMode}>
-        Toggle {colorMode === "light" ? "Dark" : "light"}
-      </Button>
-
+      <Flex justifyContent="flex-end" width="100%" padding="4">
+        <IconButton
+          onClick={toggleColorMode}
+          icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+          aria-label="Toggle theme"
+        />
+      </Flex>
       <Flex
         as="header"
         direction={{ base: "column", md: "row" }}
         alignItems="center"
         justifyContent="center"
         width="100%"
-        padding="16"
+        paddingTop="16"
       >
         <Image
           src="/assets/C0830_121-2 - web.jpg"
@@ -32,17 +44,78 @@ const Header = () => {
           borderRadius="full"
           objectFit="cover"
           objectPosition="top"
+          aspectRatio={1}
           marginRight={{ md: "4" }}
           marginBottom={{ base: "4", md: "0" }}
+          border={"1px"}
+          borderColor={"gray.800"}
         />
         <VStack align={{ base: "center", md: "start" }} spacing="1">
-          <Text fontSize="4xl" fontWeight="bold">
+          <Text fontSize="5xl" fontWeight="bold">
             Jamie Benstead
           </Text>
-          <Text fontSize="md" textAlign={{ base: "center", md: "left" }}>
-            Some intro text goes here....
+          <Text fontSize="l" textAlign={{ base: "center", md: "left" }}>
+            <Text>
+              I'm a <Box as="b">Full Stack Developer</Box> with over{" "}
+              <Box as="b">6 years</Box> of experience - I enjoy building{" "}
+              <Box as="i">interactive websites</Box> using tools such as{" "}
+              <Box as="u">React, NextJS, NodeJS, and GraphQL.</Box>
+            </Text>
           </Text>
         </VStack>
+      </Flex>
+
+      <Flex
+        direction={{ base: "column", md: "row" }}
+        alignItems="center"
+        justifyContent="center"
+        width="100%"
+        paddingTop="4"
+        gap={4}
+      >
+        <HStack
+          as="a"
+          href="https://github.com/jamiebenstead"
+          target="_blank"
+          rel="noreferrer"
+          sx={{
+            transition: "transform 0.2s, color 0.2s",
+            color: hoverColor,
+            _hover: { transform: "scale(1.1)", color: hoverColor },
+          }}
+        >
+          <Icon as={FaGithubSquare} boxSize="6" />
+          <Text>Github</Text>
+        </HStack>
+
+        <HStack
+          as="a"
+          href="https://www.linkedin.com/in/jamie-benstead-2b947112a/"
+          target="_blank"
+          rel="noreferrer"
+          sx={{
+            transition: "transform 0.2s, color 0.2s",
+            color: hoverColor,
+            _hover: { transform: "scale(1.1)", color: hoverColor },
+          }}
+        >
+          <Icon as={BsLinkedin} boxSize="6" />
+          <Text>LinkedIn</Text>
+        </HStack>
+
+        <HStack
+          as="a"
+          href="/CV.pdf"
+          download={true}
+          sx={{
+            transition: "transform 0.2s, color 0.2s",
+            color: hoverColor,
+            _hover: { transform: "scale(1.1)", color: hoverColor },
+          }}
+        >
+          <Icon as={HiDownload} boxSize="6" />
+          <Text>Download Resume</Text>
+        </HStack>
       </Flex>
     </>
   );
