0bb31d64c13375fb44651f31ba7aaf68b9a993cd
jamiebenstead <jamie.benstead@gmail.com>
2024-11-15 13:12:16 +0000

    Add SectionBox hover and load in effects

--- src/components/SectionBox.tsx
+++ src/components/SectionBox.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Box, BoxProps } from "@chakra-ui/react";
 import { useBgColor } from "../utils/themeUtils";
+import { motion } from "framer-motion";
 
 const SectionBox = ({ children }: BoxProps) => {
   const bgColor = useBgColor();
 
   return (
     <Box
-      as="section"
+      as={motion.section}
       padding="8"
       bg={bgColor}
       borderRadius="lg"
@@ -15,6 +16,10 @@ const SectionBox = ({ children }: BoxProps) => {
       maxW="container.lg"
       mx="auto"
       my="8"
+      whileHover={{ scale: 1.02 }}
+      transition="0.2s linear"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
     >
       {children}
     </Box>
